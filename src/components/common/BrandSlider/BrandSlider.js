0949bf1d1712ddb7021aa7f7882b2e5395adb692
Johan Spekman <johanspekman@gmail.com>
2021-10-05 20:26:37 +0200

    Add file structure for the brand slider

---
+++ src/components/common/BrandSlider/BrandSlider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./BrandSlider.scss";
+
+
+function BrandSlider() {
+    return (
+        <section>
+
+        </section>
+    );
+}
+
+export default BrandSlider;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,5 +3,13 @@ import Header from "./Header";
 import PaymentMethods from "./PaymentMethods";
 import Footer from "./Footer";
 import LegalTermsBanner from "./LegalTermsBanner";
+import BrandSlider from "./BrandSlider";
 
-export { Usp, Header, PaymentMethods, Footer, LegalTermsBanner };
+export {
+    Usp,
+    Header,
+    PaymentMethods,
+    Footer,
+    LegalTermsBanner,
+    BrandSlider
+};
