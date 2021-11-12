b3f225aba151956670d096553f827e420002a550
Johan Spekman <johan.spekman@kega.nl>
2021-11-12 14:57:48 +0100

    Add input to the user account form

--- src/components/Form/MyAccount/UserInfoForm/UserInfoForm.js
+++ src/components/Form/MyAccount/UserInfoForm/UserInfoForm.js
@@ -3,96 +3,82 @@ import "./UserInfoForm.scss";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 import nl from "date-fns/locale/nl";
+import moment from "moment";
 
 function UserInfoForm() {
-  const [startDate, setStartDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(new Date());
 
-  return (
+    return (
 
-      <section className="form-row">
-          <section className="col-75">
-              <section className="form-container">
-                  <form>
-                      <section className="form-row">
-                          <section className="col-75">
-                              <h2>Persoonlijke gegevens</h2>
-                              <p>Naam: </p>
-                              <input type="text" name="firstname" placeholder="Voornaam"/>
-                              <input type="text" name="lastname" placeholder="Achternaam"/>
-                          </section>
-                      </section>
-                      <section className="col-75">
-                          <p className="dateofbirth-label">Geboortedatum:</p>
-                          <DatePicker
-                            selected={startDate}
-                            onChange={date => setStartDate(date)}
-                            locale={nl}
-                            dateFormat="dd/MM/yyyy"
-                          />
-                          <section className="gender-section">
-                              <p className="gender-label">Geslacht: </p>
-                              <select name="gender" id="gender">
-                                  <option value="male">Man</option>
-                                  <option value="female">Vrouw</option>
-                                  <option value="other-gender">Anders</option>
-                              </select>
-                          </section>
-                      </section>
+        <section className="form-row">
+            <section className="col-75">
+                <section className="form-container">
+                    <form>
+                        <section className="form-row">
+                            <section className="col-75">
+                                <h2>Persoonlijke gegevens</h2>
+                                <p>Naam: </p>
+                                <input type="text" name="firstname" placeholder="Voornaam" />
+                                <input type="text" name="lastname" placeholder="Achternaam" />
+                            </section>
+                        </section>
 
-                  </form>
-              </section>
-          </section>
-      </section>
+                        <section className="form-row">
+                            <section className="col-75 personal-info">
+                                <section className="dateofbirth-form">
+                                    <p className="dateofbirth-label">Geboortedatum:</p>
+                                    <section className="form-date-picker">
+                                        <DatePicker
+                                        selected={startDate}
+                                        onChange={date => setStartDate(date)}
+                                        locale={nl}
+                                        dateFormat="dd/MM/yyyy"
+                                        maxDate={moment().format("dd/MM/yyyy")}
+                                    />
+                                    </section>
+                                </section>
+                                <section className="gender-section">
+                                    <p className="gender-label">Geslacht: </p>
+                                    <select name="gender" id="gender" className="gender-select">
+                                        <option value="male">Man</option>
+                                        <option value="female">Vrouw</option>
+                                        <option value="other-gender">Anders</option>
+                                    </select>
+                                </section>
+                            </section>
+                        </section>
 
+                        <section className="street-info-container">
+                            <section className="street-info">
+                                <section className="street-section">
+                                <p>Straat en huisnummer:  </p>
+                                <input className="street-input" type="text" name="street" placeholder="Straatnaam en huisnummer"/>
+                                </section>
+                            </section>
+                        </section>
 
+                        <section className="form-row">
+                            <section className="col-75 living-area-info">
+                            <section className="postal-info">
+                                <section className="postalcode-input">
+                                    <p>Postcode: </p>
+                                    <input type="text" name="postalcode" placeholder="Postcode" />
+                                </section>
+                                </section>
+                                <section className="city-info">
+                                    <section className="city-input">
+                                        <p>Plaatsnaam: </p>
+                                        <input type="text" name="city" placeholder="Plaatsnaam" />
+                                    </section>
+                                </section>
+                            </section>
+                            </section>
 
-
-
-
-    // <>
-    //   <form className="userinfo-form">
-    //     <section className="personal-info">
-    //       <span className="name-label">Naam: </span>
-    //       <fieldset>
-    //         <input
-    //           type="text"
-    //           className="my-account-input"
-    //           id="firstName"
-    //           placeholder="Voornaam"
-    //         />
-    //       </fieldset>
-    //       <fieldset>
-    //         <input
-    //           type="text"
-    //           className="my-account-input my-account-input-insertion"
-    //           id="insertion"
-    //           placeholder="Tussenvoegsel"
-    //         />
-    //       </fieldset>
-    //       <fieldset>
-    //         <input
-    //           type="text"
-    //           className="my-account-input"
-    //           id="lastName"
-    //           placeholder="Achternaam"
-    //         />
-    //       </fieldset>
-    //     </section>
-
-    //     <section className="secondary-info">
-    //       <fieldset>
-    //         <label htmlFor="date-of-birth" className="dob-label">
-    //           Geboortedatum
-    //           <DatePicker
-    //             selected={startDate}
-    //             onChange={(date) => setStartDate(date)}
-    //           />
-    //         </label>
-    //       </fieldset>
-    //     </section>
-    //   </form>
-    // </>
-  );
+                    </form>
+                </section>
+            </section>
+        </section>
+    );
 }
 
 export default UserInfoForm;
