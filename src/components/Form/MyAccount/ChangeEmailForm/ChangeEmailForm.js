ca1c2b16b3ef8e5b206d9d1ec6a4404deeb02877
Johan Spekman <johanspekman@gmail.com>
2021-11-13 11:08:43 +0100

    Get current user email and put is as value for the form

--- src/components/Form/MyAccount/ChangeEmailForm/ChangeEmailForm.js
+++ src/components/Form/MyAccount/ChangeEmailForm/ChangeEmailForm.js
@@ -1,19 +1,23 @@
 import React, {useState} from 'react';
 import "./ChangeEmailForm.scss";
 import ChangeEmailButton from "../../Buttons/ChangeEmailButton";
+import {useAuth} from "../../../../contexts/AuthContext";
 
 function ChangeEmailForm() {
 
     const [currentEmail, setCurrentEmail] = useState('');
     const [newEmail, setNewEmail] = useState('');
 
+    const auth = useAuth();
+    const currentUserEmail = auth.currentUser.email;
+
     return (
         <section className="change-email-container">
             <section className="change-email-form">
                 <p>Wijzig uw email: </p>
                 <input className="change-email-input" type="text" name="current-email"
-                       placeholder="Huidig email-address"
                         onChange={(event => setCurrentEmail(event.target.value))}
+                        value={currentUserEmail}
                 />
                 <input className="change-email-input" type="text" name="new-email" placeholder="Nieuw email-address"
                         onChange={(event => setNewEmail(event.target.value))}
