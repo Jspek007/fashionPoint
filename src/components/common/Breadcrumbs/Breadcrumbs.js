334bc4807d51db41f8c5a871027831115af9f392
Johan Spekman <johanspekman@gmail.com>
2021-11-18 21:06:45 +0100

    Add breadcrumb styling

--- src/components/common/Breadcrumbs/Breadcrumbs.js
+++ src/components/common/Breadcrumbs/Breadcrumbs.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Breadcrumbs.scss";
 import { Link } from "react-router-dom";
 
 const Breadcrumbs = ({ crumbs }) => {
-    if (crumbs.length <= 1) {
-        return null;
-    }
+  if (crumbs.length <= 1) {
+    return null;
+  }
 
-    return (
-        <section>
-            {crumbs.map(({ name, path}, key) =>
-                key + 1 === crumbs.length ? (
-                    <span key={key}>
-                        {name}
-                    </span>
-                ) : (
-                    <Link key={key} to={path}>
-                        {name}
-                    </Link>
-                )
-            )}
-        </section>
-    );
+  return (
+    <section className="breadcrumbs">
+      {crumbs.map(({ name, path }, key) =>
+        key + 1 === crumbs.length ? (
+          <span className="breadcrumb__active" key={key}>
+            {name}
+          </span>
+        ) : (
+          <Link className="breadcrumb__item" key={key} to={path}>
+            {name}
+          </Link>
+        )
+      )}
+    </section>
+  );
 };
 
 export default Breadcrumbs;
