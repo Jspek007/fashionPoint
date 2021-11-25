d40a709c58533179256566b2162fb3d564bcf856
Johan Spekman <johanspekman@gmail.com>
2021-11-25 18:14:07 +0100

    Refactor forgot password button

--- src/components/Form/Buttons/ResetPasswordButton/index.js
+++
@@ -1,3 +0,0 @@
-import ResetPasswordButton from "./ResetPasswordButton";
-
-export default ResetPasswordButton;
--- src/utils/firebaseErrors.js
+++ src/utils/firebaseErrors.js
@@ -6,5 +6,6 @@ export const firebaseErrors = {
     'auth/email-already-in-use' : 'Opgegeven email heeft al een account.',
     'auth/too-many-requests' : 'U heeft te vaak geprobeerd om in te loggen. Probeer het later opnieuw.',
     'auth/requires-recent-login' : 'U dient opnieuw in te loggen om uw gegevens te wijzigen.',
-    'auth/weak-password' : 'Wachtwoord dient minimaal 6 tekens te bevatten',
+    'auth/weak-password' : 'Wachtwoord dient minimaal 6 tekens te bevatten.',
+    'auth/missing-email' : 'Invoerveld is leeg. Vul uw email in.',
 }
