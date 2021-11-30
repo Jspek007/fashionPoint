c27ca2b29f60dd73cf9041f23fa5b76dd4881bae
Johan Spekman <johanspekman@gmail.com>
2021-11-30 21:09:37 +0100

    Add dropdown to cart icon

--- src/components/common/Minicart/MinicartMenu/MinicartMenu.js
+++ src/components/common/Minicart/MinicartMenu/MinicartMenu.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import "./MinicartMenu.scss";
 import {FaShoppingCart} from "react-icons/fa"
+import {useDetectOutsideClick} from "../../../../helpers/UseDetectOutsideClick/UseDetectOutsideClick";
 
 const MinicartMenu = () => {
+
+    const dropdownRef = useRef(null);
+    const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef);
+    const onClick = () => setIsActive(!isActive);
+
     return (
         <section className="menu-container">
-            <button className="menu-trigger">
+            <button className="menu-trigger" onClick={onClick}>
                <span>
                    <FaShoppingCart className="user-icon"/>
                </span>
+                <section ref={dropdownRef} className={`minicart-menu ${isActive ? `active` : `inactive`}`}>
+                    <h1>Hallo</h1>
+                </section>
             </button>
         </section>
     );
