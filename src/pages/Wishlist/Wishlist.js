25e9f689cff5188be2bfbce65b6aea514cf8f46b
Johan Spekman <johanspekman@gmail.com>
2021-12-13 19:39:05 +0100

    Add empty wishlist content to prevent errors

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import {ProductCards} from "../../components/common";
 import "./Wishlist.scss";
-import {Title} from "../../components/common/Content/TextComponents";
+import {SubTitle, Title} from "../../components/common/Content/TextComponents";
 
 function Wishlist() {
 
     let wishlistArray = JSON.parse(localStorage.getItem('wishlist'))
 
-    return (
-        <>
-            <section className="wishlist-title-container">
-                <Title text="Mijn wishlist" />
-            </section>
-            <section className="wishlist-product-container">
-                <ProductCards data={wishlistArray}/>
-            </section>
-        </>
-    );
+    if (wishlistArray.length === 0) {
+        return (
+            <>
+                <section className="wishlist-title-container">
+                    <Title text="Mijn wishlist"/>
+                </section>
+                <section className="wishlist-content">
+                    <SubTitle text="Geen producten gevonden in uw wishlist. Voeg items toe om ze hier te zien." />
+                </section>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <section className="wishlist-title-container">
+                    <Title text="Mijn wishlist"/>
+                </section>
+                <section className="wishlist-product-container">
+                    {!wishlistArray && (
+                        <h1>Wishlist is leeg. Snel, voeg iets toe!</h1>
+                    )}
+                    {wishlistArray && (
+                        <ProductCards data={wishlistArray}/>
+                    )}
+                </section>
+            </>
+        );
+    }
 }
 
 export default Wishlist;
