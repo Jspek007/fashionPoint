e2d01ff07f4b6430991cecbe1fe35d3893373387
Johan Spekman <johanspekman@gmail.com>
2021-12-20 10:49:15 +0100

    Make add to cart sticky on mobile

--- src/components/common/Catalog/ProductDetail/ProductDetail.js
+++ src/components/common/Catalog/ProductDetail/ProductDetail.js
@@ -1,102 +1,104 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import { useParams } from "react-router";
+import {useParams} from "react-router";
 import "./ProductDetail.scss";
 import AddToCartButton from "../../../Form/Buttons/AddToCartButton";
 import ToggleWishlistIcon from "../ToggleWishlistIcon";
 import ProductDetailSkeleton from "../../SkeletonLoader/ProductDetailSkeleton";
 import CartModal from "../../Modal/CartModal";
-import { SubTitle } from "../../Content/TextComponents";
-import { FaShoppingCart } from "react-icons/fa";
-import { RedirectButton } from "../../../Form/Buttons";
-import { Link } from "react-router-dom";
+import {SubTitle} from "../../Content/TextComponents";
+import {FaShoppingCart} from "react-icons/fa";
+import {RedirectButton} from "../../../Form/Buttons";
+import {Link} from "react-router-dom";
 
 function ProductDetail() {
-  const [loading, setLoading] = useState(false);
-  const [productData, setProductData] = useState([]);
-  const { productId } = useParams();
-  const getSingleProductApi = `https://fakestoreapi.com/products/${productId}`;
-  let cartArray = localStorage.getItem("currentCart");
-  const [modal, setModal] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [productData, setProductData] = useState([]);
+    const {productId} = useParams();
+    const getSingleProductApi = `https://fakestoreapi.com/products/${productId}`;
+    let cartArray = localStorage.getItem("currentCart");
+    const [modal, setModal] = useState(false);
 
-  useEffect(() => {
-    const fetchSpecificProduct = async () => {
-      setLoading(true);
-      try {
-        const result = await axios.get(getSingleProductApi);
-        setProductData(result.data);
-      } catch (error) {
-        console.log(error);
-      }
+    useEffect(() => {
+        const fetchSpecificProduct = async () => {
+            setLoading(true);
+            try {
+                const result = await axios.get(getSingleProductApi);
+                setProductData(result.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchSpecificProduct().then(() => {
+            setLoading(false);
+        });
+    }, [getSingleProductApi]);
+
+    const addProductToCart = () => {
+        cartArray = JSON.parse(localStorage.getItem("currentCart"))
+            ? JSON.parse(localStorage.getItem("currentCart"))
+            : [];
+
+        let objectIndex = cartArray.findIndex((obj) => obj.id === productData.id);
+        if (objectIndex === -1) {
+            let newCartProduct = {
+                title: productData.title,
+                price: productData.price,
+                image: productData.image,
+                id: productData.id,
+                qty: 1,
+            };
+            cartArray.push(newCartProduct);
+            localStorage.setItem("currentCart", JSON.stringify(cartArray));
+        } else {
+            cartArray[objectIndex].qty++;
+            localStorage.setItem("currentCart", JSON.stringify(cartArray));
+        }
+        setModal(true);
     };
-    fetchSpecificProduct().then(() => {
-      setLoading(false);
-    });
-  }, [getSingleProductApi]);
 
-  const addProductToCart = () => {
-    cartArray = JSON.parse(localStorage.getItem("currentCart"))
-      ? JSON.parse(localStorage.getItem("currentCart"))
-      : [];
-
-    let objectIndex = cartArray.findIndex((obj) => obj.id === productData.id);
-    if (objectIndex === -1) {
-      let newCartProduct = {
-        title: productData.title,
-        price: productData.price,
-        image: productData.image,
-        id: productData.id,
-        qty: 1,
-      };
-      cartArray.push(newCartProduct);
-      localStorage.setItem("currentCart", JSON.stringify(cartArray));
-    } else {
-      cartArray[objectIndex].qty++;
-      localStorage.setItem("currentCart", JSON.stringify(cartArray));
-    }
-    setModal(true);
-  };
-
-  return (
-    <>
-      {loading && <ProductDetailSkeleton />}
-      {!loading && (
-        <section className="product-detail-container">
-          <section className="product-image-container">
-            <ToggleWishlistIcon productData={productData} />
-            <img
-              src={productData.image}
-              className="product-image"
-              alt={productData.title}
-            />
-          </section>
-          <section className="add-to-cart-container">
-            <AddToCartButton
-              clickHandler={addProductToCart}
-              specificProductData={productData}
-            />
-          </section>
-          <section className="product-details">
-            <section className="product-detail-title">
-              {productData.title}
-            </section>
-            <section className="product-detail-description">
-              {productData.description}
-            </section>
-            <CartModal show={modal} handleModal={setModal}>
-              <section className="modal-content">
-                <FaShoppingCart className="modal-icon" />
-                <SubTitle text="Product is toegevoegd aan uw winkelwagen." />
-                <Link exact="true" to="/winkelwagen">
-                  <RedirectButton callToAction="Bekijken" primary />
-                </Link>
-              </section>
-            </CartModal>
-          </section>
-        </section>
-      )}
-    </>
-  );
+    return (
+        <>
+            {loading && <ProductDetailSkeleton/>}
+            {!loading && (
+                <>
+                    <section className="product-detail-title">
+                        {productData.title}
+                    </section>
+                    <section className="product-detail-container">
+                        <section className="product-image-container">
+                            <ToggleWishlistIcon productData={productData}/>
+                            <img
+                                src={productData.image}
+                                className="product-image"
+                                alt={productData.title}
+                            />
+                        </section>
+                        <section className="add-to-cart-container">
+                            <AddToCartButton
+                                clickHandler={addProductToCart}
+                                specificProductData={productData}
+                            />
+                        </section>
+                        <section className="product-details">
+                            <section className="product-detail-description">
+                                {productData.description}
+                            </section>
+                            <CartModal show={modal} handleModal={setModal}>
+                                <section className="modal-content">
+                                    <FaShoppingCart className="modal-icon"/>
+                                    <SubTitle text="Product is toegevoegd aan uw winkelwagen."/>
+                                    <Link exact="true" to="/winkelwagen">
+                                        <RedirectButton callToAction="Bekijken" primary/>
+                                    </Link>
+                                </section>
+                            </CartModal>
+                        </section>
+                    </section>
+                </>
+            )}
+        </>
+    );
 }
 
 export default ProductDetail;
--- src/components/common/SkeletonLoader/ProductDetailSkeleton.js
+++ src/components/common/SkeletonLoader/ProductDetailSkeleton.js
@@ -3,24 +3,26 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const ProductDetailSkeleton = () => {
-  return (
-    <section className="product-detail-container">
-      <section className="product-image-container">
-        <Skeleton width={380} height={500} />
-      </section>
-      <section className="product-details">
-        <section className="product-detail-title">
-          <Skeleton width={500} height={80} />
-        </section>
-        <section className="product-detail-description">
-          <Skeleton width={500} height={250} />
-        </section>
-        <section className="bottom-detail-container">
-          <Skeleton width={500} height={100} />
-        </section>
-      </section>
-    </section>
-  );
+    return (
+        <>
+            <section className="product-detail-title">
+                <Skeleton width={1000} height={35} />
+            </section>
+            <section className="product-detail-container">
+                <section className="product-image-container">
+                    <Skeleton width={380} height={450}/>
+                    <section className="bottom-detail-container">
+                        <Skeleton width={380} height={100}/>
+                    </section>
+                </section>
+                <section className="product-details">
+                    <section className="product-detail-title">
+                        <Skeleton width={550} height={585}/>
+                    </section>
+                </section>
+            </section>
+        </>
+    );
 };
 
 export default ProductDetailSkeleton;
