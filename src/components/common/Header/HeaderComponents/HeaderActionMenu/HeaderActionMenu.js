118cdfb5dfc85e76577f29e1e30df1ed694cb994
Johan Spekman <johanspekman@gmail.com>
2021-12-20 19:20:20 +0100

    Cart styling for mobile

--- src/components/common/Header/HeaderComponents/HeaderActionMenu/HeaderActionMenu.js
+++ src/components/common/Header/HeaderComponents/HeaderActionMenu/HeaderActionMenu.js
@@ -4,7 +4,7 @@ import {RedirectButton} from "../../../../Form/Buttons";
 import {useAuth} from "../../../../../contexts/AuthContext";
 import {Link} from "react-router-dom";
 import MinicartMenu from "../../Minicart/MinicartMenu";
-import {FaUser} from "react-icons/fa";
+import {FaShoppingCart, FaUser} from "react-icons/fa";
 
 function HeaderActionMenu() {
 
