ec8d5c56011fcd1e267f32646c101ae53ab691c6
Johan Spekman <johanspekman@gmail.com>
2021-12-23 19:55:42 +0100

    Finalize usp banners for mobile

--- src/components/common/Content/UspBanner/UspBanner.js
+++ src/components/common/Content/UspBanner/UspBanner.js
@@ -5,7 +5,7 @@ import {useInView} from "react-intersection-observer";
 const UspBanner = ({imageSrc, textTitle, seoText, flipped}) => {
 
     const {ref, inView} = useInView({
-        threshold: 0.2,
+        threshold: 0.4,
         triggerOnce: true,
     });
 
