67b1082d4c927dc0af936ab501f802657d387fd6
Johan Spekman <johanspekman@gmail.com>
2021-12-27 19:54:31 +0100

    Finalize mobile checkout

--- src/pages/Checkout/PaymentPage/PaymentPage.js
+++ src/pages/Checkout/PaymentPage/PaymentPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Title from "../../../components/common/Content/TextComponents/Title/Title";
 import CheckoutSummary from "../../../components/common/Checkout/CheckoutSummary/CheckoutSummary";
 import CheckoutInputItem from "../../../components/common/Checkout/CheckoutInputs/CheckoutInputItem";
@@ -8,67 +8,72 @@ import GooglePay from "../../../assets/images/paymentMethods/Google_pay.svg";
 import MasterCard from "../../../assets/images/paymentMethods/Mastercard.svg";
 import Paypal from "../../../assets/images/paymentMethods/Paypal.svg";
 import FormButtonContainer from "../../../components/Form/Forms/FormComponents/FormButtonContainer";
-import {RedirectButton} from "../../../components/Form/Buttons";
-import Link from "react-router-dom/es/Link";
+import { RedirectButton } from "../../../components/Form/Buttons";
+import { Link } from "react-router-dom";
 
 const PaymentPage = () => {
+  let cartData = JSON.parse(localStorage.getItem("checkoutData"))
+    ? JSON.parse(localStorage.getItem("checkoutData"))
+    : [];
+  let shippingMethod = JSON.parse(localStorage.getItem("shippingMethod"))
+    ? JSON.parse(localStorage.getItem("shippingMethod"))
+    : [];
 
-    let cartData = JSON.parse(localStorage.getItem("checkoutData")) ? JSON.parse(localStorage.getItem("checkoutData"))
-        : [];
-    let shippingMethod = JSON.parse(localStorage.getItem("shippingMethod")) ? JSON.parse(localStorage.getItem("shippingMethod"))
-        : [];
+  const createOrder = () => {
+    localStorage.removeItem("currentCart");
+    localStorage.removeItem("shippingMethod");
+  };
 
-    const createOrder = () => {
-        localStorage.removeItem("currentCart");
-        localStorage.removeItem("shippingMethod");
-    }
+  return (
+    <section className="checkout-container">
+      <Title text="Verzenden naar: " />
 
-    return (
-        <section className="checkout-container">
-            <Title text="Verzenden naar: "/>
+      <CheckoutSummary cartData={cartData} shippingMethod={shippingMethod} />
 
-            <CheckoutSummary cartData={cartData} shippingMethod={shippingMethod}/>
+      <Title text="Betaalmethode: " />
+      <section className="inputs-container">
+        <CheckoutInputItem
+          imageSource={Klarna}
+          inputText="Klarna"
+          payment="payment"
+          inputName="payment"
+        />
+        <CheckoutInputItem
+          imageSource={ApplePay}
+          inputText="ApplePay"
+          payment="payment"
+          inputName="payment"
+        />
+        <CheckoutInputItem
+          imageSource={GooglePay}
+          inputText="GooglePay"
+          payment="payment"
+          inputName="payment"
+        />
+        <CheckoutInputItem
+          imageSource={MasterCard}
+          inputText="MasterCard"
+          payment="payment"
+          inputName="payment"
+        />
+        <CheckoutInputItem
+          imageSource={Paypal}
+          inputText="PayPal"
+          payment="payment"
+          inputName="payment"
+        />
+      </section>
+      <FormButtonContainer>
+        <Link exact="true" to="/checkout/success">
+          <RedirectButton
+            callToAction="Afrekenen"
+            primary
+            clickHandler={createOrder}
+          />
+        </Link>
+      </FormButtonContainer>
+    </section>
+  );
+};
 
-            <Title text="Betaalmethode: "/>
-            <section className="inputs-container">
-                <CheckoutInputItem
-                    imageSource={Klarna}
-                    inputText="Klarna"
-                    payment="payment"
-                    inputName="payment"
-                />
-                <CheckoutInputItem
-                    imageSource={ApplePay}
-                    inputText="ApplePay"
-                    payment="payment"
-                    inputName="payment"
-                />
-                <CheckoutInputItem
-                    imageSource={GooglePay}
-                    inputText="GooglePay"
-                    payment="payment"
-                    inputName="payment"
-                />
-                <CheckoutInputItem
-                    imageSource={MasterCard}
-                    inputText="MasterCard"
-                    payment="payment"
-                    inputName="payment"
-                />
-                <CheckoutInputItem
-                    imageSource={Paypal}
-                    inputText="PayPal"
-                    payment="payment"
-                    inputName="payment"
-                />
-            </section>
-            <FormButtonContainer>
-                <Link exact="true" to="/checkout/success">
-                    <RedirectButton callToAction="Afrekenen" primary clickHandler={createOrder}/>
-                </Link>
-            </FormButtonContainer>
-        </section>
-    )
-}
-
-export default PaymentPage
+export default PaymentPage;
